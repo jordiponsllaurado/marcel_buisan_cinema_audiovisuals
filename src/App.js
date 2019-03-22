2dc45dbb045cc49ac5c78a6044f34dd3c9f17d2c
Jordi Pons <jordi.pons@netcentric.biz>
2019-03-22 20:53:08 +0100

    Add video

--- src/App.js
+++ src/App.js
@@ -2,25 +2,17 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/header/Header'
+import YouTube from 'react-youtube';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <div className='Body'>
+          <YouTube
+            videoId={'uGPSUvaWryA'}
+          />
         </div>
       </div>
     );
