cd8916bbf7d53346c92e31d0fa7ccba1c16870b6
Jordi Pons <jordi.pons@netcentric.biz>
2019-03-22 19:57:19 +0100

    Initial commit

---
+++ src/components/header/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './Header.css';
+
+class Header extends Component {
+    render() {
+        return (
+            <div className='header'>Marcel Buisan / cinema i audiovisual</div>
+        )
+    }
+}
+
+export default Header
