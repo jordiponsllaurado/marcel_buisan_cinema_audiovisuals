10100e2e132210f30e69a1217513073a24458c7e
Jordi Pons <jordi.pons@netcentric.biz>
2019-03-24 20:10:53 +0100

    Updates

---
+++ precache-manifest.bd4715e3c63f30c695b4629c57958006.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d80d2ff9046f603e6e86",
+    "url": "/marcel_buisan_cinema_audiovisuals/static/css/main.7c9173af.chunk.css"
+  },
+  {
+    "revision": "d80d2ff9046f603e6e86",
+    "url": "/marcel_buisan_cinema_audiovisuals/static/js/main.276a062c.chunk.js"
+  },
+  {
+    "revision": "84cce426f6539579e11b",
+    "url": "/marcel_buisan_cinema_audiovisuals/static/js/runtime~main.e52351c2.js"
+  },
+  {
+    "revision": "520da0b72804a2ae88b4",
+    "url": "/marcel_buisan_cinema_audiovisuals/static/js/2.c93a9835.chunk.js"
+  },
+  {
+    "revision": "0d407c84e3f8268ce77c8f15c801f03c",
+    "url": "/marcel_buisan_cinema_audiovisuals/index.html"
+  }
+];
