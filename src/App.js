8dfed33971f48c43af42bf9bed2b940ddb8b8ceb
jordi.pons <jordi.pons@typeform.com>
2019-03-27 09:40:27 +0100

    Adds material UI

--- src/App.js
+++ src/App.js
@@ -4,27 +4,28 @@ import Footer from './components/footer/Footer'
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Home from './components/home/Home';
 import About from './components/about/About';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 
 class App extends Component {
   render() {
     return (
-        <div className='Body'>
+        <div>
           <Router>
-            <div className>
-              <div className='header'>
-                Marcel Buisan / cinema i audiovisual
-                <ul>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/about">About</Link>
-                </li>
-                </ul>
-              </div>
-
-                <Route exact path="/" component={Home} />
-                <Route path="/about" component={About} />
+            <div className='root'>
+              <AppBar position="static">
+                <Toolbar>
+                  <Typography variant="h6" color="inherit" className='grow'>
+                      Marcel Buisan / cinema i audiovisual
+                  </Typography>
+                  <Button color="inherit"><Link style={{ textDecoration: 'none', color: 'inherit' }} to="/">Home</Link></Button>
+                  <Button color="inherit"><Link style={{ textDecoration: 'none', color: 'inherit' }} to="/about">About</Link></Button>
+                </Toolbar>
+              </AppBar>
+              <Route exact path="/" component={Home} />
+              <Route path="/about" component={About} />
             </div>
         </Router>
         <Footer />
--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -6,9 +6,9 @@ class Footer extends Component {
     render() {
         return (
             <div className='footer'>
-                <SocialIcon className='social_icon' style={{ height: 25, width: 25 }} url="https://twitter.com/MarcelBuisan" network="twitter" bgColor='#616283'/>
-                <SocialIcon className='social_icon' style={{ height: 25, width: 25 }} url="https://www.linkedin.com/in/marcel-buisan-comellas-73342059/" network="linkedin" bgColor='#616283'/>
-                <SocialIcon className='social_icon' style={{ height: 25, width: 25 }} url="hhttps://www.youtube.com/user/mbuisan" network="youtube" bgColor='#616283'/>
+                <SocialIcon className='social_icon' style={{ height: 25, width: 25 }} url="https://twitter.com/MarcelBuisan" network="twitter" bgColor='#243073'/>
+                <SocialIcon className='social_icon' style={{ height: 25, width: 25 }} url="https://www.linkedin.com/in/marcel-buisan-comellas-73342059/" network="linkedin" bgColor='#243073'/>
+                <SocialIcon className='social_icon' style={{ height: 25, width: 25 }} url="hhttps://www.youtube.com/user/mbuisan" network="youtube" bgColor='#243073'/>
             </div>
         )
     }
