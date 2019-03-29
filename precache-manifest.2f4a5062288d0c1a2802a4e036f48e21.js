6134ca882ab082792d7c191e4d1e4e3deb80c9f1
Jordi Pons <jordi.pons@netcentric.biz>
2019-03-29 17:59:53 +0100

    Updates

---
+++ precache-manifest.2f4a5062288d0c1a2802a4e036f48e21.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "466ba03c83cd19421658",
+    "url": "/marcel_buisan_cinema_audiovisuals/static/css/main.a697bb93.chunk.css"
+  },
+  {
+    "revision": "466ba03c83cd19421658",
+    "url": "/marcel_buisan_cinema_audiovisuals/static/js/main.f88e6328.chunk.js"
+  },
+  {
+    "revision": "84cce426f6539579e11b",
+    "url": "/marcel_buisan_cinema_audiovisuals/static/js/runtime~main.e52351c2.js"
+  },
+  {
+    "revision": "c13db4efd33afa2d27a0",
+    "url": "/marcel_buisan_cinema_audiovisuals/static/css/2.dae8a8ab.chunk.css"
+  },
+  {
+    "revision": "c13db4efd33afa2d27a0",
+    "url": "/marcel_buisan_cinema_audiovisuals/static/js/2.856e87f9.chunk.js"
+  },
+  {
+    "revision": "10cfdd2c949109d6db61ce5aa3cd0682",
+    "url": "/marcel_buisan_cinema_audiovisuals/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/marcel_buisan_cinema_audiovisuals/precache-manifest.0de335f732e113dcc140db0cedaca10f.js"
+  "/marcel_buisan_cinema_audiovisuals/precache-manifest.2f4a5062288d0c1a2802a4e036f48e21.js"
 );
 
 workbox.clientsClaim();
